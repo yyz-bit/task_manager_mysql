27f30ea044ac4bbc5015aaae2907322d2fe1153e
yyz <231210853+yyz-bit@users.noreply.github.com>
2026-08-03 17:06:44 +0800

    refactor: 使用环境变量加载数据库配置

--- database.py
+++ database.py
@@ -1,14 +1,20 @@
+import os
 from getpass import getpass
+
 import pymysql
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 def create_connection():
-    mysql_password = getpass("MySQL password: ")
+    mysql_password = os.getenv("DB_PASSWORD") or getpass("MySQL password: ")
     connection = pymysql.connect(
-        host="127.0.0.1",
-        port=3306,
-        user="root",
+        host=os.getenv("DB_HOST", "127.0.0.1"),
+        port=int(os.getenv("DB_PORT", "3306")),
+        user=os.getenv("DB_USER", "task_app"),
         password=mysql_password,
-        database="task_manager_mysql",
+        database=os.getenv("DB_NAME", "task_manager_mysql"),
         charset="utf8mb4",
         cursorclass=pymysql.cursors.DictCursor,
     )
