427125dd269181c44ac290921e41fd80e255fd23
yyz <231210853+yyz-bit@users.noreply.github.com>
2026-08-06 20:35:33 +0800

    test: 新增了display_task_logs的测试

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,4 +1,5 @@
 from db import display_tasks
+from db import display_task_logs
 
 
 def test_display_tasks_shows_completed_task(capsys):
@@ -37,4 +38,27 @@ def test_display_tasks_shows_unfinished_task(capsys):
     captured = capsys.readouterr()
     assert captured.out == (
         "99. [未完成] 测试任务 | 用户：zhangsan | 分类：Python\n"
+    )
+
+
+def test_display_task_logs(capsys):
+    logs = [
+        {
+            "id": 12,
+            "action_type": "created",
+            "details": "创建任务",
+            "created_at": "2026-08-03 20:02:46",
+        },
+        {
+            "id": 13,
+            "action_type": "updated",
+            "details": "修改任务信息",
+            "created_at": "2026-08-03 20:04:58",
+        }
+    ]
+    display_task_logs(logs)
+    captured = capsys.readouterr()
+    assert captured.out == (
+        "12. [created] 创建任务 | 时间：2026-08-03 20:02:46\n"
+        "13. [updated] 修改任务信息 | 时间：2026-08-03 20:04:58\n"
     )
